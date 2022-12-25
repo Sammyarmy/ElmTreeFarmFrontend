d46529fae13c683554c4046dd685e67ccd34f852
Sam <samarmstrong1000@gmail.com>
2022-12-25 22:43:54 +0000

    readd a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+test('renders elm tree farm', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Elm tree farm/i);
+  expect(linkElement).toBeInTheDocument();
+});
